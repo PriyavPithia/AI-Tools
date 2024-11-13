bdb51e3f5cb23a16ed0745523c57c774ac744939
Richard-Abraham <richardabraham.kay.37@gmail.com>
2024-11-14 01:40:17 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,9 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true
   },
-  // Add this for debugging
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 2,
-  },
+  output: 'standalone'
 };
 
 export default nextConfig;
